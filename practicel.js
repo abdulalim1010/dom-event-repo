b62668e8e61c24e76bc3456e2336df969d22fdc7
abdulalilm1010 <abdulalim243457@gmail.com>
2025-03-13 23:07:16 +0600

    new item on the pathe

---
+++ practicel.js
@@ -0,0 +1,10 @@
+const aar = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+function sum(aar) {
+  console.log(aar)
+  
+  for (let index = 0; index < Array.length; index++){
+    const element = aar[index]
+    console.log(element);
+  }
+}
+sum(aar);
