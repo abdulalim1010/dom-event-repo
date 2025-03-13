295562179800ccb42ec8023d67659473f31ecb99
abdulalilm1010 <abdulalim243457@gmail.com>
2025-03-13 23:48:17 +0600

    last item

--- practicel.js
+++ practicel.js
@@ -1,10 +1,15 @@
 const aar = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 function sum(aar) {
+ 
+  
+  for (let index = 0; index < aar.length; index++){
+    const element = aar[index]
+   
+    aar[index] = element *element
+  }
   console.log(aar)
   
-  for (let index = 0; index < Array.length; index++){
-    const element = aar[index]
-    console.log(element);
-  }
 }
+console.log(aar)
 sum(aar);
+console.log(aar)
